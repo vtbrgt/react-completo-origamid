dbac8e9e32d6da6a93890f68ccf5e6403478f12b
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-10-05 10:39:59 -0300

    exercising useEffect

--- hooks/src/useEffect/App.jsx
+++ hooks/src/useEffect/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import Produto from './Produto';
 
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
+
 const App = () => {
-  const [ativo, setAtivo] = React.useState(false);
+  const [dados, setDados] = React.useState(null);
+
+  React.useEffect(() => {
+    if (localStorage.getItem('produto'))
+      setDados(localStorage.getItem('produto'));
+  }, []);
+
+  function handleClick({ target }) {
+    setDados(target.innerText);
+  }
+
+  React.useEffect(() => {
+    localStorage.setItem('produto', [dados]);
+  }, [dados]);
 
   return (
-    <div>
-      {ativo && <Produto />}
-      <button onClick={() => setAtivo(!ativo)}>
-        {!ativo ? 'Ativar' : 'Desativar'}
-      </button>
-    </div>
+    <>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+      <Produto produto={dados} />
+    </>
   );
 };
 
--- hooks/src/useEffect/Produto.jsx
+++ hooks/src/useEffect/Produto.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
-const Produto = () => {
+const Produto = ({ produto }) => {
+  const [info, setInfo] = React.useState(null);
+
   React.useEffect(() => {
-    function handleScroll(event) {
-      console.log(event);
+    if (produto != null) {
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+        .then((response) => response.json())
+        .then((json) => setInfo(json));
     }
+  }, [produto]);
 
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  return (
-    <div style={{ height: '200vh' }}>
-      <p>Meu produto</p>
-    </div>
-  );
+  if (info) {
+    return (
+      <div>
+        <h1>{info.nome}</h1>
+        <p>{info.descricao}</p>
+        <p>R$ {info.preco}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Produto;
