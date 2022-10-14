d00804a8d8d4ff7939cbcfbcfeb558bb4e15e23b
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-10-14 16:58:18 -0300

    starting React form with input

---
+++ forms/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Input from './Input';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Input />
+  </React.StrictMode>
+);
