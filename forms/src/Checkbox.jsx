1a24b9d0bbaa3ea4457557d6dec5d74122876014
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-10-17 09:45:49 -0300

    checkbox input

---
+++ forms/src/Checkbox.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Checkbox = () => {
+  const [cores, setCores] = React.useState(['vermelho']);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
+
+  return (
+    <form>
+      <label>
+        <input
+          type="checkbox"
+          value="azul"
+          checked={handleChecked('azul')}
+          onChange={handleChange}
+        />
+        Azul
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="vermelho"
+          checked={handleChecked('vermelho')}
+          onChange={handleChange}
+        />
+        Vermelho
+      </label>
+    </form>
+  );
+};
+
+export default Checkbox;
--- forms/src/index.jsx
+++ forms/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Radio from './Radio';
+import Checkbox from './Checkbox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Radio />
+    <Checkbox />
   </React.StrictMode>
 );
