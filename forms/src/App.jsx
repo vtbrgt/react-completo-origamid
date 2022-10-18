6fe101da6ca7948b2d30670fa1edcf472a40590f
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-10-18 10:24:33 -0300

    select component

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Input from './Components/Input';
+import Select from './Components/Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
+  const [produto, setProduto] = React.useState('');
 
   return (
     <form>
-      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
-      <Input
-        type="password"
-        id="email"
-        label="Email"
-        value={email}
-        setValue={setEmail}
+      <Select
+        options={['Smartphone', 'Tablet']}
+        value={produto}
+        setValue={setProduto}
       />
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
+      <Input id="email" label="Email" value={email} setValue={setEmail} />
       <button>Enviar</button>
     </form>
   );
