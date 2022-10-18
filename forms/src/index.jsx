fd33b6f7004e51eb6fc2ab905f32c909f0550bc0
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-10-18 10:16:18 -0300

    input component

--- forms/src/index.jsx
+++ forms/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Checkbox from './Checkbox';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Checkbox />
+    <App />
   </React.StrictMode>
 );
