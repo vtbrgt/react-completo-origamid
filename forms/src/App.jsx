2a3bf22b880fa3297aa4913ae03428f4ad575d2a
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-10-20 09:58:17 -0300

    radio components

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Input from './Components/Input';
+import Radio from './Components/Radio';
 import Select from './Components/Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('Vermelho');
+  const [frutas, setFrutas] = React.useState('');
 
   return (
     <form>
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+
+      <h2>Frutas</h2>
+      <Radio
+        options={['Limão', 'Laranja', 'Uva']}
+        value={frutas}
+        setValue={setFrutas}
+      />
+
       <Select
         options={['Smartphone', 'Tablet']}
         value={produto}
