853a3bf209672bdfbadbe3485f7887f01e9c11b0
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-12-12 19:11:33 -0300

    useForm
    created custom hook 'useForm' to dinamically validate input fields

--- forms/src/Validacao.jsx
+++ forms/src/Validacao.jsx
@@ -1,35 +1,16 @@
-import React from 'react'
-import Input from './Components/Input'
+import React from 'react';
+import Input from './Components/Input';
+import useForm from './useForm';
 
 const Validacao = () => {
-  const [cep, setCep] = React.useState('')
-  const [error, setError] = React.useState(null)
-
-  function validateCep(value) {
-    if (value.length === 0) {
-      setError('Preencha um valor')
-      return false
-    } else if(!/^\d{5}-?\d{3}$/.test(value)) {
-      setError('Preencha um CEP válido')
-      return false
-    } else {
-      setError(null)
-      return true
-    }
-  }
-
-  function handleBlur({target}) {
-    validateCep(target.value)
-  }
-
-  function handleChange({target}) {
-    if(error) validateCep(target.value)
-    setCep(target.value)
-  }
+  const cep = useForm('cep');
+  const email = useForm('email');
+  const nome = useForm();
+  const sobrenome = useForm(false);
 
   function handleSubmit(event) {
-    event.preventDefault()
-    if(validateCep(cep)) {
+    event.preventDefault();
+    if (cep.validate() && email.validate() && nome.validate()) {
       console.log('Enviou');
     } else {
       console.log('Não enviou');
@@ -38,11 +19,19 @@ const Validacao = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Input label='CEP' id='CEP' type='text' placeholder='00000-000' value={cep} onChange={handleChange} onBlur={handleBlur} />
-      {error && <p>{error}</p>}
+      <Input label="Nome" id="nome" type="text" {...nome} />
+      <Input label="Sobrenome" id="sobrenome" type="text" {...sobrenome} />
+      <Input
+        label="CEP"
+        id="CEP"
+        type="text"
+        placeholder="00000-000"
+        {...cep}
+      />
+      <Input label="Email" id="email" type="email" {...email} />
       <button>Enviar</button>
     </form>
-  )
-}
+  );
+};
 
-export default Validacao
+export default Validacao;
