5b024e0dfd472999c77d993eae6e14e6a51d72af
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-12-13 09:45:41 -0300

    css modules

--- css/src/App.jsx
+++ css/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Title from './Title';
+import Produto from './CSSModules/Produto';
 
 const App = () => {
   return (
-    <div className="container">
-      <Title />
-    </div>
+    <>
+      <Produto />
+    </>
   );
 };
 
