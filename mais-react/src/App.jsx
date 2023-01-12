fa579b94a97511170be55afb245792b2fceea1d3
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-01-12 11:01:27 -0300

    tipando props com PropTypes

---
+++ mais-react/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mais React</h1>
+      <Button margin="20px">Clique Aqui</Button>
+    </div>
+  );
+}
+
+export default App;
