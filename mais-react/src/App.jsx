bc91ec8af1ee3e12ea8aad120e277b6790712f09
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-01-24 09:02:03 -0300

    react useReducer

--- mais-react/src/App.jsx
+++ mais-react/src/App.jsx
@@ -1,16 +1,31 @@
 import './App.css';
 import React from 'react';
 import Header from './Header';
+import Exemplo from './Exemplo';
 // import Contato from './Contato';
 //const Contato = React.lazy(() => import('./Contato'));
 
+function reducer(state, action) {
+  switch (action) {
+    case 'aumentar':
+      return state + 1;
+    case 'diminuir':
+      return state - 1;
+    default:
+      throw new Error();
+  }
+}
+
 function App() {
-  const [contar, setContar] = React.useState(0);
+  const [state, dispatch] = React.useReducer(reducer, 0);
 
   return (
     <div className="App">
       <Header />
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+      <p>{state}</p>
+      <button onClick={() => dispatch('aumentar')}>+</button>
+      <button onClick={() => dispatch('diminuir')}>-</button>
+      <Exemplo />
     </div>
   );
 }
